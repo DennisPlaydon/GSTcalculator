a7102496c938246e34b086440bff19be682f0cf6
DennisPlaydon <37030744+DennisPlaydon@users.noreply.github.com>
2018-06-28 22:11:40 +1200

    writing WORKS to file when value is greater than 0

--- GSTcalculator.py
+++ GSTcalculator.py
@@ -1,3 +1,17 @@
 import openpyxl
 
-wb = openpyxl.load_workbook('C:\\Users\\Dennis\\Desktop\\taxprac')
+wb = openpyxl.load_workbook('C:\\Users\\Dennis\\Desktop\\taxprac.xlsx')
+sheet = wb['Sheet1']
+for i in range(2, sheet.max_row):
+    try:
+        if 'Z Ormiston R' in sheet['F'+str(i)].value:
+            #sheet['K'+str(i)] = 1
+            sheet['K'+str(i)] = 'WORKS'
+           
+        elif (sheet['H'+str(i)].value) > 0:
+            #sheet['K'+str(i)] = -1
+            sheet['K'+str(i)] = 'WORKS'
+    except Exception as exc:
+        continue
+
+wb.save('C:\\Users\\Dennis\\Desktop\\gst_copy.xlsx')
